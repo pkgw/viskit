95d8a70700a51f9d865956495aa92301a9c029f2
Peter Williams <peter@newton.cx>
2009-11-06 16:48:20 -0800

    dsls.c: use the new dataset routines to provide much more info

--- demo/dsls.c
+++ demo/dsls.c
@@ -5,6 +5,7 @@ int
 main (int argc, char **argv)
 {
     Dataset *ds;
+    DSItemInfo *dii;
     GSList *items, *iter;
     GError *err = NULL;
 
@@ -26,8 +27,62 @@ main (int argc, char **argv)
     }
 
     for (iter = items; iter; iter = iter->next) {
-	printf ("%s\n", (char *) iter->data);
+	gchar *name = (gchar *) iter->data;
+	gint64 ival;
+	gdouble dval;
+
+	dii = ds_probe_item (ds, name, &err);
+
+	if (dii == NULL) {
+	    fprintf (stderr, "Error probing item \"%s\" in \"%s\": %s\n",
+		     name, argv[1], err->message);
+	    return 1;
+	}
+
+	printf ("%-10s", dii->name);
+
+	if (dii->is_large)
+	    printf ("large");
+	else
+	    printf ("small");
+
+	printf (" %-12s %10d vals", ds_type_names[dii->type], dii->nvals);
+
+	if (dii->nvals == 1) {
+	    switch (dii->type) {
+	    case DST_I8:
+	    case DST_I16:
+	    case DST_I32:
+	    case DST_I64:
+		if (ds_get_item_i64 (ds, name, &ival))
+		    printf (" = [unreadable?!]");
+		else
+		    printf (" = %" G_GINT64_FORMAT, ival);
+		break;
+	    case DST_F32:
+	    case DST_F64:
+		if (ds_get_item_f64 (ds, name, &dval))
+		    printf (" = [unreadable?!]");
+		else
+		    printf (" = %f", dval);
+		break;
+	    default:
+		break;
+	    }
+	} else if (!dii->is_large && dii->type == DST_I8) {
+	    gchar *s = ds_get_item_short_string (ds, name);
+
+	    if (s == NULL)
+		printf (" = [unstringable?!]");
+	    else {
+		printf (" = \"%s\"", s);
+		g_free (s);
+	    }
+	}
+
+	printf ("\n");
 	g_free (iter->data);
+	ds_item_info_free (dii);
     }
 
     g_slist_free (items);
